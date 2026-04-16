f91549b300062d79c7c25a8e488baa37ad3b385c
rakesh <tripathy10138@gmail.com>
2026-04-16 20:30:08 +0530

    fix contribution

--- 6.object.js
+++ 6.object.js
@@ -29,5 +29,23 @@ userdata.greeting = function() {
     console.log(`Hello my name is ${this.name} and I am ${this.age} years old and I live in ${this.city} and my hobbies are ${this.hobbies.join(", ")}.`);
 }
 
-console.log(userdata.greeting());   
-console.log(userdata.greeting);
+console.log(userdata.greeting());   // this is a method that we have added to the object userdata, which is a function that can be called to get the greeting message.
+console.log(userdata.greeting);  // this is a function that we have added to the object userdata, which is a function that can be called to get the greeting message. 
+// but it will not execute the function it will just return the function definition.
+
+const user1={
+    name: "Rakesh Chandra Tripathy",
+}
+const user2={
+    name: "Rakesh Chandra Tripathy2"
+}
+const user3={
+    name: "Rakesh Chandra Tripathy3",
+}
+
+const user4=Object.assign({}, user1, user2, user3); // this is a method that we can use to merge multiple objects into one object.
+console.log(user4); // this will return the merged object with the properties of all the objects that we have merged.
+
+// we can also use the spread operator to merge multiple objects into one object.
+const user5={...user1, ...user2, ...user3}; // this is a method that we can use to merge multiple objects into one object.
+console.log(user5); // this will return the merged object with the properties of all the objects that we have merged.
