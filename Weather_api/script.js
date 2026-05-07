ca820b241001b2d401efd5c560e407c1bb1bd956
rakesh <tripathy10138@gmail.com>
2026-05-08 01:23:20 +0530

    updated javascript project

--- Weather_api/script.js
+++ Weather_api/script.js
@@ -9,7 +9,6 @@ searchBtn.addEventListener('click', async function(){
 
     const city = cityInput.value;
 
-   // const apiKey = "4a0e4e7f361a44cb2ed38a3b7253af02";
 
     const url =
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
