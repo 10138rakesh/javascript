d9c6cb0f5a09c457f94f0fd845b369c3540bc3fc
rakesh <tripathy10138@gmail.com>
2026-07-05 23:20:07 +0530

    updated javascript project

--- javascript_basic/10.scope.js
+++ javascript_basic/10.scope.js
@@ -15,5 +15,7 @@ test();
 
 //block Scope only accessible inside the block   
 ///****   let and const are the block scope  whereas the var is function-scoped*/
+//the block scope is a feature of JavaScript that allows variables to be defined within a specific block of code, such as an if statement or a loop. This means that the variable is only accessible within that block and cannot be accessed outside of it. This helps to prevent naming conflicts and makes the code easier to read and maintain. In contrast, function-scoped variables are accessible throughout the entire function in which they are defined, which can lead to unintended consequences if not managed carefully.
+//therefore , it is generally recommended to use block-scoped variables (let and const) whenever possible, as they provide better encapsulation and reduce the risk of naming conflicts. However, there may be cases where function-scoped variables (var) are necessary, such as when working with legacy code or when defining variables that need to be accessed across multiple functions. In these cases, it is important to use clear and descriptive variable names to avoid confusion and ensure that the code is easy to understand.
 
 
