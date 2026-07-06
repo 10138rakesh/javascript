8266c96c2ffa27f5c7316698d7a8dfa721513faa
rakesh <tripathy10138@gmail.com>
2026-07-06 23:37:09 +0530

    updated javascript project

--- javascript_basic/9.function.js
+++ javascript_basic/9.function.js
@@ -3,6 +3,9 @@ function sum(a,b){
 }
 sum(3,5);
 
+//this is a function that takes two parameters, a and b, and returns their sum. The function is then called with the arguments 3 and 5, which will return 8.
+// therefore , the output of this code will be 8.
+
 
 
 
